3c5f20fbd9982f87558c2d85389343b5e06d9d53
Alex Kor <Skoryakin3@gmail.com>
2021-10-03 17:20:06 +0300

    fix 1.3.1

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -6,7 +6,7 @@ function ProfileCard() {
   <div className="profile_card">
     <div className="firstinfo"><img src="https://sun9-44.userapi.com/impg/Nelcg59BxI2d5xGxCArNwiwsQyGvUs9hiczFlA/_H5-HtjqWX4.jpg?size=1804x2160&quality=96&sign=6f45f93d9fd8b647130a8a20001484ca&type=album"/>
       <div className="profileinfo">
-        <h1>Alex Koryakin</h1>
+        <h1>Alexander Koryakin</h1>
         <h3></h3>
         <p className="bio">Lorem Lorem</p>
       </div>
