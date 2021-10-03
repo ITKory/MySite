4d75ea07240b5ba73242f48e109870d1d4989ded
Alex Kor <Skoryakin3@gmail.com>
2021-10-03 17:05:10 +0300

    fix1

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -2,17 +2,17 @@
 function ProfileCard() {
     return (
 
-<div class="profile_content">
-  <div class="profile_card">
-    <div class="firstinfo"><img src="https://sun9-44.userapi.com/impg/Nelcg59BxI2d5xGxCArNwiwsQyGvUs9hiczFlA/_H5-HtjqWX4.jpg?size=1804x2160&quality=96&sign=6f45f93d9fd8b647130a8a20001484ca&type=album"/>
-      <div class="profileinfo">
+<div className="profile_content">
+  <div className="profile_card">
+    <div className="firstinfo"><img src="https://sun9-44.userapi.com/impg/Nelcg59BxI2d5xGxCArNwiwsQyGvUs9hiczFlA/_H5-HtjqWX4.jpg?size=1804x2160&quality=96&sign=6f45f93d9fd8b647130a8a20001484ca&type=album"/>
+      <div className="profileinfo">
         <h1>Alex Koryakin</h1>
         <h3></h3>
-        <p class="bio">Lorem Lorem</p>
+        <p className="bio">Lorem Lorem</p>
       </div>
     </div>
   </div>
-  <div class="badgescard"><a href="https://github.com/ITKory"><img src="https://cdn.worldvectorlogo.com/logos/github-icon.svg" width="25px"/></a></div>
+  <div className="badgescard"><a href="https://github.com/ITKory"><img src="https://cdn.worldvectorlogo.com/logos/github-icon.svg" width="25px"/></a></div>
 </div>
     )
 }
