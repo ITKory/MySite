e760538944486f020a624e54ceb51661f254201f
Alex Kor <Skoryakin3@gmail.com>
2021-11-12 22:14:43 +0300

    p

--- pages/pen.js
+++ pages/pen.js
@@ -2,7 +2,7 @@
 function Pen(){
     return(
         <>
-        <iframe height="600"  className="Pen" scrolling="no"  src="https://codepen.io/itkory/embed/WNjKyXQ?default-tab=html%2Cresult&editable=true&theme-id=dark" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+        <iframe height="600"  className="Pen" scrolling="no"  src="https://codepen.io/itkory/embed/WNjKyXQ?default-tab=html%2Cresult&editable=true&theme-id=dark" frameBorder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
         See the Pen <a href="https://codepen.io/itkory/pen/WNjKyXQ">
         Work With R&amp;M Api</a> by  Alexander (<a href="https://codepen.io/itkory">@itkory</a>)
         on <a href="https://codepen.io">CodePen</a>.
