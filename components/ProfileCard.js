119c3d36266e0ea841f0448b6f82b2e9a72daa34
Alex Kor <Skoryakin3@gmail.com>
2022-04-02 16:46:13 +0300

    ChangePhoto

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -1,22 +1,22 @@
-import mainPhoto from'../public/images/kbO_K0ZymJA.jpg'
-import gitLogo from'../public/images/github-icon.svg';
+import mainPhoto from '../public/images/lcE3stJ1cSE.jpg'
+import gitLogo from '../public/images/github-icon.svg';
 function ProfileCard() {
   return (
 
     <div className="profile_content">
       <div className="profile_card">
-        <div className="firstinfo"><img src={mainPhoto['src']}  />
+        <div className="firstinfo"><img src={mainPhoto['src']} />
           <div className="profileinfo">
             <h1>Alexander Koryakin</h1>
             <h3></h3>
-            <p className="bio"><h5>PROGRAMMING SKILLS</h5><div>HTML/CSS</div> 
-            <div>ReactJS</div>
-             <div>React Native</div>
-             <div>JQ</div>
-             <div>Redux</div>
-             <div>C#</div>
-            <div>design patterns</div>
-             </p>
+            <p className="bio"><h5>PROGRAMMING SKILLS</h5><div>HTML/CSS</div>
+              <div>ReactJS</div>
+              <div>React Native</div>
+              <div>JQ</div>
+              <div>Redux</div>
+              <div>C#</div>
+              <div>design patterns</div>
+            </p>
             <p className="bio"><h5> PROFESSIONAL SKILLS </h5>I am communicative, hardworking and responsible person</p>
           </div>
         </div>
